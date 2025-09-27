070255d18921802752b8cfc91e8eb388e0e47078
Rohit <sharmarv561@gmail.com>
2025-09-26 22:18:08 -0500

    Added README with installation instructions

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='DiamondPricePrediction',
     version='0.0.1',
-    author='Krish',
-    author_email='krish.naik@pw.live',
+    author='Rohit',
+    author_email='sharmarv561@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 
